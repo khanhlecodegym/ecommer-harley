ec97a746feb6d0cba3cd7da14729c2a84ac98378
khanhlecodegym <quockhanhk16tpm@gmail.com>
2021-09-05 21:28:03 +0700

    fc

---
+++ ecommerce-harley-backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b4bd09ce1e0c96a07a58459bb5d258ba'),
+    },
+  },
+});
---
+++ ecommerce-harley-backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '11eb90f0-9d31-4314-886b-ae877297e27d'
+};
